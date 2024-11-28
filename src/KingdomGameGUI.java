005bc0b03557311fda2119e1c3f3a86c9376943c
berryc00kie <102135801+berryc00kie@users.noreply.github.com>
2024-11-28 21:52:59 +0900

    Update KingdomGameGUI.java

--- src/KingdomGameGUI.java
+++ src/KingdomGameGUI.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 //import KingdomGame.KingdomGamePanel;
 
 public class KingdomGameGUI {
-    public static final String IMAGE_PATH = "image폴더경로\\";
+    public static final String IMAGE_PATH = "src\\image폴더경로\\";
     
     public static void main(String[] args) {
         KingdomGamePanel panel = new KingdomGamePanel(IMAGE_PATH);
